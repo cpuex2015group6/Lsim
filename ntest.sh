27d2cb8a10edef73d05e746bfc047faf503b0b10
Liva <sap.pcmail@gmail.com>
2016-01-22 22:15:26 +0900

    fix path

--- ntest.sh
+++ ntest.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 cur=`pwd`
-pushd ../cpuex/min-caml/test && cat ../libmincaml.ml globals.ml min-rt.ml > hoge.ml && cd .. && make byte-code && ./min-caml test/hoge > ./output && mv test/hoge.s ${cur}/hoge.s && popd && ./native.sh hoge.s
+pushd ../compiler/min-caml/test && cat ../libmincaml.ml globals.ml min-rt.ml > hoge.ml && cd .. && make byte-code && ./min-caml test/hoge > ./output && mv test/hoge.s ${cur}/hoge.s && popd && ./native.sh hoge.s
